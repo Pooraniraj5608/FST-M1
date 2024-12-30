1be0ed7b04b1cb28fb8fff46ccbb7845d77614cd
Pooraniraj5608 <poorani1030@gmail.com>
2024-12-30 18:51:00 +0800

    Add files via upload

--- JUnit/Activities/Activity1.java
+++ JUnit/Activities/Activity1.java
@@ -1,9 +1,13 @@
 package activities;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertIterableEquals;
 
 public class Activity1 {
     // Test fixtures
@@ -20,29 +24,41 @@ public class Activity1 {
     // Test method to test the insert operation
     @Test
     public void insertTest() {
+    	//Expected List
+    	List<String> expectedList = Arrays.asList("alpha","beta","gamma");
+    	
         // Assertion for size
         assertEquals(2, list.size(), "Wrong size");
         // Add new element
-        list.add(2, "charlie");
+        list.add(2, "gamma");
         // Assert with new elements
         assertEquals(3, list.size(), "Wrong size");
+        
+        //Assert list items
+        assertIterableEquals(expectedList,list);
 
         // Assert individual elements
         assertEquals("alpha", list.get(0), "Wrong element");
         assertEquals("beta", list.get(1), "Wrong element");
-        assertEquals("charlie", list.get(2), "Wrong element");
+        assertEquals("gamma", list.get(2), "Wrong element");
     }
 
     // Test method to test the replace operation
     @Test
     public void replaceTest() {
+    	
+    	//Expected List
+    	List<String> expectedList = Arrays.asList("alpha","beta","charlie");
+    	
+    	 assertEquals(2, list.size());
         // Replace new element
-        list.set(1, "charlie");
+        list.add(2, "gamma");
+        list.set(2, "charlie");
 
         // Assert size of list
-        assertEquals(2, list.size(), "Wrong size");
-        // Assert individual elements
-        assertEquals("alpha", list.get(0), "Wrong element");
-        assertEquals("charlie", list.get(1), "Wrong element");
+        assertEquals(3, list.size(), "Wrong size");
+        
+      //Assert list items
+        assertIterableEquals(expectedList,list);
     }
 }
---
+++ JUnit/Activities/NotEnoughFundsException.java
@@ -0,0 +1,9 @@
+package activities;
+
+public class NotEnoughFundsException extends RuntimeException {
+	
+	public NotEnoughFundsException(Integer amount, Integer balance) {
+		super("Attempted to withdraw"+ amount + "with balance of " + balance);
+	}
+
+}
